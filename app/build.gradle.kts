4b7bc2ac9b03bc9a6dc4f531dff732602e14eb47
João Pedro Barbosa <joppemdesu@gmail.com>
2024-08-11 16:58:16 -0400

    Primeira etapa finalizada, configurações aplicadas, e inicio do desenvolvimento da UI de usuário

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,10 +29,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures{
+        viewBinding;
+    }
 }
 
 dependencies {
 
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
